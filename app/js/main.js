e9c68109fc3e31da3c22ffb17abf1735f2325b23
Sergey <serg.k199311@mail.ru>
2022-03-04 20:41:22 +0600

    init: create header

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
-$(function () {
-    $('body').css('background-color', 'green')
+const headerBtn = document.querySelector('.header-btn');
+const hideNavMenu = document.querySelector('.header-nav');
+
+headerBtn.addEventListener('click', function() {
+    this.classList.toggle('active-btn');
+    hideNavMenu.classList.toggle('active-nav');
 });
 
 
