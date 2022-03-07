8c0e1645326d4e52ad32e796e4e2f075632386cf
Sergey <serg.k199311@mail.ru>
2022-03-07 17:01:52 +0600

    cr: create modal window

--- app/js/main.js
+++ app/js/main.js
@@ -50,7 +50,7 @@ window.addEventListener('load', function () {
   });
   /* Swiper end */
 
-
+  /* Обрезание строки отзывов начало */
 
   Ellipsis({
     ellipsis: '…',
@@ -61,5 +61,40 @@ window.addEventListener('load', function () {
     portrait: null,
     break_word: false,
   });
+  /* Обрезание строки отзывов  конец */
+
+
+  const modalBtns = document.querySelectorAll('.btn');
+  const modalsBg = document.querySelector('.modal');
+  const modalsForm = document.querySelector('.modal__form');
+  const closseModal = document.querySelector('.modal__close');
+
+  modalBtns.forEach(function (el) {
+    el.addEventListener('click', function (e) {
+      modalsBg.classList.add('modal-active-2');
+      modalsForm.classList.add('modal-active-1');
+    });
+  })
+
+  closseModal.addEventListener('click', function () {
+    modalsBg.classList.remove('modal-active-2');
+    modalsForm.classList.remove('modal-active-1');
+  });
+
+  document.addEventListener('click', function(e) {
+      if (e.target === modalsBg) {
+        modalsBg.classList.remove('modal-active-2');
+        modalsForm.classList.remove('modal-active-1');
+      }
+  });
+
 
 });
+
+
+
+
+
+
+
+
